6d8f7fe54011c3c9d7c3032b97b5de0afc984b67
HCY <HCY_NULL@163.com>
2019-05-11 23:12:32 +0800

    完成全局变量定义翻译

--- C0/mips.cpp
+++ C0/mips.cpp
@@ -12,7 +12,7 @@ midcode g_curMidcode;
 class _constType{
     public:
     string name;
-    string value;
+    int value;
 };
 
 vector<_constType> g_constDef;
@@ -33,13 +33,13 @@ void globalConst(){
     _getMidcode();
     while(true){
         if(g_curMidcode.s_operation == "const"){
-            // generate mips order
-            cout << g_curMidcode.s_result << ":\t.word\t" << g_curMidcode.s_betaVar << endl;
             // add to global const defination vector
             _constType tempArg;
             tempArg.name = g_curMidcode.s_result;
             tempArg.value = atoi(g_curMidcode.s_betaVar.c_str());
             g_constDef.push_back(tempArg);
+            // generate mips order
+            cout << g_curMidcode.s_result << ":\t.word\t" << g_curMidcode.s_betaVar << endl;
             // get next midcode
             _getMidcode();
         }
@@ -51,14 +51,39 @@ void globalConst(){
 
 
 void globalVar(){
-    
+    while(true){
+        if(g_curMidcode.s_operation == "int" || g_curMidcode.s_operation == "char"){
+            // add to global variable defination
+            _constType tempArg;
+            tempArg.name = g_curMidcode.s_result;
+            tempArg.value = atoi(g_curMidcode.s_betaVar.c_str());
+            g_constDef.push_back(tempArg);
+            //generate mips order
+            cout << g_curMidcode.s_result << ":\t.space\t" << "4" << endl;
+            _getMidcode();
+        }
+        else if(g_curMidcode.s_operation == "inta" || g_curMidcode.s_operation == "chara"){
+            // add to global variable defination
+            _constType tempArg;
+            tempArg.name = g_curMidcode.s_result;
+            tempArg.value = 0;
+            g_constDef.push_back(tempArg);
+            //generate mips order
+            int size = atoi(g_curMidcode.s_betaVar.c_str());
+            cout << g_curMidcode.s_result << ":\t.space\t" << size*4 << endl;
+            _getMidcode();
+        }
+        else{
+            break;
+        }
+    }
 }
 
 
 void mips_start(){
     cout << ".data" << endl;
     globalConst();
-
+    globalVar();
 }
 
 
