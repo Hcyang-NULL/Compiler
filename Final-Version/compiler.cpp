87da0b6845a0773ef0eac9fc0db8515c368ab1cd
HCY <HCY_NULL@163.com>
2019-07-01 18:04:44 +0800

    小修报告

--- Final-Version/compiler.cpp
+++ Final-Version/compiler.cpp
@@ -30,13 +30,15 @@ int main(int argc,char *argv[]){
     else{
         file = ".\\Final-Version\\test.c";
     }
+    // lex analysis
     cout << "Lex analysis result: " << endl;
     vector<pair<string, TYPE> > vec_tempFuncarg_1;
     cout << file << endl;
     word_initialize(file);
     vec_tempFuncarg_1 = word_analyze();
+
+    // grammar analysis + quaternary production
     cout << endl << "Grammar analysis result: " << endl;
-    // vector<struct inter_code> vec_tempFuncarg_2;
     int g_tempError = 0;
     grammar_initialize(vec_tempFuncarg_1);
     g_tempError = grammar_analyze();
@@ -45,11 +47,14 @@ int main(int argc,char *argv[]){
         cout << endl;
         g_test();
     }
+
+    // optimize
     opt optimizer;
     vector<midcode> vec_tempFuncarg_2 = getVecMidcodes();
     optimizer._DFG_Analysis(vec_tempFuncarg_2);
     vector<optMidcode> vec_tempFuncarg_3 = optimizer.getFinalOptMidcodes();
 
+    // transfer format
     vector<midcode> vec_tempFuncarg_4;
     bool start = false;
     int gap_index = 0;
@@ -110,7 +115,7 @@ int main(int argc,char *argv[]){
         }
         
     }
-
+    // output the result of optmize operation
     cout << "----------------------------" << endl;
     for(int i = 0; i < vec_tempFuncarg_4.size(); i++)
     {
@@ -118,20 +123,11 @@ int main(int argc,char *argv[]){
         std::cout << "< " << now.s_operation << ", " << now.s_alphaVar << ", " << now.s_betaVar << ", " << now.s_result << ">" << endl;
     }
     cout << "----------------------------" << endl;
-    for(int i = 0; i < vec_tempFuncarg_2.size(); i++)
-    {
-        midcode now = vec_tempFuncarg_2[i];
-        std::cout << "< " << now.s_operation << ", " << now.s_alphaVar << ", " << now.s_betaVar << ", " << now.s_result << ">" << endl;
-    }
-    cout << "----------------------------" << endl;
-    // for(int i = 0; i < vec_tempFuncarg_3.size(); i++)
-    // {
-    //     optMidcode now = vec_tempFuncarg_3[i];
-    //     std::cout << "< " << now.s_operation << ", " << now.s_alphaVar << ", " << now.s_betaVar << ", " << now.s_result << ">" << endl;
-    // }
-    // opt::_exprOptimizer(vec_tempFuncarg_2);
-    // system("pause");.
+
+    // translate
     translate(vec_tempFuncarg_4);
+
+    // end
     system("pause");
     return 0;
 }
--- Final-Version/stastic.py
+++ Final-Version/stastic.py
@@ -1,30 +1,37 @@
 import os
 
-# lines = 0
+lines = 0
 
-# files = os.walk('.')
-# for now_dir in files:
-#     for file in now_dir[2]:
-#         if file.endswith('.cpp') or file.endswith('.h'):
-#             with open(file, "r", encoding='utf-8') as f:
-#                 try:
-#                     for temp in f.readlines():
-#                         lines = lines+1
-#                 except Exception:
-#                     pass
+files = os.walk('.')
+for now_dir in files:
+    for file in now_dir[2]:
+        if file.endswith('.cpp') or file.endswith('.h'):
+            print(file, end='')
+            cost = 0
+            with open(file, "r", encoding='utf-8') as f:
+                try:
+                    for temp in f.readlines():
+                        if temp != "\n":
+                            cost = cost+1
+                except Exception:
+                    print('fuck')
+                    pass
+            print(": "+str(cost))
+            lines += cost
                 
 
-# print('total line: ')
-# print(lines)
+print('total line: ')
+print(lines)
 
-file = 'config'
+file = 'optimizer'
 
 lines = 0
 
-# with open(file+'.cpp',  encoding='utf-8') as f:
-#     for line in f.readlines():
-#         if line != '\n':
-#             lines += 1
+if os.path.isfile(file+'.cpp'):
+    with open(file+'.cpp',  encoding='utf-8') as f:
+        for line in f.readlines():
+            if line != '\n':
+                lines += 1
 
 if os.path.isfile(file+'.h'):
     with open(file+'.h',  encoding='utf-8') as f:
